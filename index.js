d71b3d85ba31ab6b7f69ff33da4eba73de320d7c
Uyen Tran <thtuyen1011@gmail.com>
2025-09-06 00:17:08 -0700

    Write js script

---
+++ index.js
@@ -0,0 +1,19 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 30 * 2 - 20;
+
+let code3 = (32 % 39) + 7;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+
+console.log(codeA);
+
+console.log(codeB);
